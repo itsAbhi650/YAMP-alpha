c3a9d07545f012b912ce2d6e23215b85a217a503
itsAbhi650 <abhinavp650@gmail.com>
2021-11-21 22:34:44 +0530

    Improved fetching current position of playing file as TimeSpan object by CSCore.Extensions.GetPosition(...)

--- YAMP-alpha/NewMain.cs
+++ YAMP-alpha/NewMain.cs
@@ -1,7 +1,6 @@
 ﻿using CSCore;
 using CSCore.Streams;
 using System.Drawing;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -9,8 +8,6 @@ namespace YAMP_alpha
 {
     public partial class NewMain : Form
     {
-
-
         YAMP_Core YAMPCore;
         //PeakMeterDialog PeakDialog = new PeakMeterDialog();
         public NewMain()
@@ -19,7 +16,7 @@ namespace YAMP_alpha
             int ClientTop = RectangleToScreen(ClientRectangle).Top;
             int height = Height - pictureBox1.Height;
             MinimumSize = new Size(387, height);
-            
+
         }
 
         private void ThreadSafeCall(MethodInvoker method)
@@ -48,9 +45,12 @@ namespace YAMP_alpha
                     if (OPD.ShowDialog() == DialogResult.OK)
                     {
                         YAMPCore.InitializePlayer(OPD.FileName);
+                        var Duration = TimeConverterFactory.Instance.GetTimeConverterForSource(YAMPCore.PlayerSource).
+                            ToTimeSpan(YAMPCore.PlayerSource.WaveFormat, YAMPCore.PlayerSource.Length).TotalSeconds;
+                        int durationS = (int)Duration + 1;
                         //DoResize(YAMPCore.TagInfo.Cover);
                         trackBar2.Value = YAMPCore.SoundOutVolume;
-                        trackBar1.Maximum = YAMPCore.PlayerLength;
+                        trackBar1.Maximum = durationS;// YAMPCore.PlayerLength;
 
                         pictureBox1.BackgroundImage = YAMPCore.TagInfo.Cover;
                     }
@@ -84,7 +84,8 @@ namespace YAMP_alpha
         {
             if (YAMPCore.PlayerPlaybackState == CSCore.SoundOut.PlaybackState.Playing)
             {
-                trackBar1.Value = YAMPCore.CurrentPosition;
+                TimeStamp Duration = Extensions.GetPosition(YAMPCore.PlayerSource);
+                trackBar1.Value = (Duration.Minutes * 60) + Duration.Seconds;
             }
         }
 
@@ -115,7 +116,7 @@ namespace YAMP_alpha
                 {
                     YAMPCore.ReleasePlayer();
                     YAMPCore.InitializePlayer(OPD.FileName);
-                    
+
                     trackBar2.Value = YAMPCore.SoundOutVolume;
                     trackBar1.Maximum = YAMPCore.PlayerLength;
                 }
@@ -129,7 +130,7 @@ namespace YAMP_alpha
         //    //Size sz = new Size(Width, Height);
         //    width = pictureBox1.Width - pictureBox1.Image.Width;
         //    Size = new Size(Width - width, height);
-            
+
         //}
 
         private void button7_Click(object sender, System.EventArgs e)
@@ -144,7 +145,7 @@ namespace YAMP_alpha
         {
             if (trackBar1.Value - 5000 >= 0)
             {
-                trackBar1.Value = trackBar1.Value-5000;
+                trackBar1.Value = trackBar1.Value - 5000;
             }
         }
 
@@ -166,7 +167,7 @@ namespace YAMP_alpha
 
         private void echoToolStripMenuItem_Click(object sender, System.EventArgs e)
         {
-            using (EchoSignalDialog ESD = new EchoSignalDialog() {Core = YAMPCore })
+            using (EchoSignalDialog ESD = new EchoSignalDialog() { Core = YAMPCore })
             {
                 ESD.ShowDialog();
             }
@@ -178,7 +179,7 @@ namespace YAMP_alpha
             //notificationSource.SingleBlockStreamAlmostFinished += NotificationSource_SingleBlockStreamAlmostFinished; ;
             //notificationSource.SingleBlockRead += NotificationSource_SingleBlockRead;
             //var PeakMeterSampleSource = notificationSource.ToWaveSource(8).ToSampleSource();
-            using (PeakMeterDialog PMD = new PeakMeterDialog() { Core = YAMPCore})
+            using (PeakMeterDialog PMD = new PeakMeterDialog() { Core = YAMPCore })
             {
                 PMD.ShowDialog();
             }
