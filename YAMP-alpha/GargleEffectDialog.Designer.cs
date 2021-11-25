60f24487249e0df77b5558c4e57d5ff37f64155d
itsAbhi650 <abhinavp650@gmail.com>
2021-11-26 01:10:30 +0530

    Created Gargle Effect Interface

---
+++ YAMP-alpha/GargleEffectDialog.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace YAMP_alpha
+{
+    partial class GargleEffectDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(0, 22);
+            this.trackBar1.Maximum = 1500;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(295, 29);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 20;
+            this.trackBar1.Value = 20;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Change the value (Hz) to observe a change in effect";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 41);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wave Shape";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Triangle",
+            "Square"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 21);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 21);
+            this.button2.TabIndex = 12;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(204, 84);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // GargleEffectDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 102);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "GargleEffectDialog";
+            this.Text = "GargleEffectDialog";
+            this.Load += new System.EventHandler(this.GargleEffectDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ YAMP-alpha/GargleEffectDialog.cs
@@ -0,0 +1,69 @@
+﻿using CSCore.Streams.Effects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YAMP_alpha
+{
+    public partial class GargleEffectDialog : Form
+    {
+        public GargleEffectDialog()
+        {
+            InitializeComponent();
+        }
+        internal DmoGargleEffect GargleEffect;
+        public GargleEffectDialog(ref DmoGargleEffect GargleEffectRef)
+        {
+            InitializeComponent();
+            GargleEffect = GargleEffectRef;
+            updatelabel();
+        }
+
+        private void GargleEffectDialog_Load(object sender, EventArgs e)
+        {
+            trackBar1.Value = GargleEffect.RateHz;
+            comboBox1.SelectedIndex = (int)GargleEffect.WaveShape;
+            checkBox1.Checked = GargleEffect.IsEnabled;
+            updatelabel();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            GargleEffect.RateHz = trackBar1.Value;
+            updatelabel();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GargleEffect.WaveShape = (GargleWaveShape)comboBox1.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = trackBar1.Value - 1;
+            updatelabel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = trackBar1.Value + 1;
+            updatelabel();
+        }
+
+        private void updatelabel()
+        {
+            label1.Text = string.Format("Change the value ({0} Hz) to observe a change in effect", GargleEffect.RateHz);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            GargleEffect.IsEnabled = checkBox1.Checked;
+        }
+    }
+}
