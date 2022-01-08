b24957b246df539f0cf736728b7d550b70d028ee
itsAbhi650 <abhinavp650@gmail.com>
2022-01-08 11:36:58 +0530

    Added Waveform (NAudio) Dialog for Left and Right Channel

---
+++ YAMP-alpha/Waveform.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace YAMP_alpha
+{
+    partial class Waveform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.waveformPainter1 = new NAudio.Gui.WaveformPainter();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.waveformPainter2 = new NAudio.Gui.WaveformPainter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // waveformPainter1
+            // 
+            this.waveformPainter1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.waveformPainter1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waveformPainter1.Location = new System.Drawing.Point(44, 0);
+            this.waveformPainter1.Margin = new System.Windows.Forms.Padding(0);
+            this.waveformPainter1.Name = "waveformPainter1";
+            this.waveformPainter1.Size = new System.Drawing.Size(481, 58);
+            this.waveformPainter1.TabIndex = 0;
+            this.waveformPainter1.Text = "waveformPainter1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.571428F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.42857F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.waveformPainter1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.waveformPainter2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(525, 117);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // waveformPainter2
+            // 
+            this.waveformPainter2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.waveformPainter2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waveformPainter2.Location = new System.Drawing.Point(44, 58);
+            this.waveformPainter2.Margin = new System.Windows.Forms.Padding(0);
+            this.waveformPainter2.Name = "waveformPainter2";
+            this.waveformPainter2.Size = new System.Drawing.Size(481, 59);
+            this.waveformPainter2.TabIndex = 1;
+            this.waveformPainter2.Text = "waveformPainter2";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "L";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "R";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Waveform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 117);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Waveform";
+            this.Text = "Waveform";
+            this.Load += new System.EventHandler(this.Waveform_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NAudio.Gui.WaveformPainter waveformPainter1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private NAudio.Gui.WaveformPainter waveformPainter2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ YAMP-alpha/Waveform.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.Streams;
+
+namespace YAMP_alpha
+{
+    public partial class Waveform : Form
+    {
+        ISampleSource Source;
+        YAMP_Core CoreSource;
+        private bool BYPASS_TMR = false;
+
+        public Waveform()
+        {
+            InitializeComponent();
+        }
+
+        public Waveform(ref YAMP_Core Core)
+        {
+            InitializeComponent();
+            CoreSource = Core;
+        }
+
+        private void Waveform_Load(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.ColumnStyles[0].SizeType = SizeType.Absolute;
+            tableLayoutPanel1.ColumnStyles[0].Width = 50;
+            timer1.Start();
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BYPASS_TMR = CoreSource.PlayerStopped || CoreSource.PlayerPaused ? true : false;
+            if (!BYPASS_TMR)
+            {
+                waveformPainter1.AddMax(CoreSource.WaveFormLEFT);
+                waveformPainter2.AddMax(CoreSource.WaveFormRIGHT);
+            }
+        }
+    }
+}
