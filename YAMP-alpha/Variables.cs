0de3cf21f9b9d0ae05bafa2ac9a1346f20f3b959
itsAbhi650 <abhinavp650@gmail.com>
2022-01-23 20:20:47 +0530

    Shifted more variables, hence a cleaner 'YAMP-Core'

--- YAMP-alpha/Variables.cs
+++ YAMP-alpha/Variables.cs
@@ -1,20 +1,33 @@
 ﻿using CSCore.Streams;
 using CSCore;
 using CSCore.CoreAudioAPI;
+using CSCore.Streams.Effects;
 
 namespace YAMP_alpha
 {
     public static class YAMPVars
     {
         public static YAMP_Core CORE;
-        public static VolumeSource VolumeSource;
+        public static MMDevice MediaDevice;
+        public static DmoCompressorEffect CompressorEffect;
+        public static DmoWavesReverbEffect WavesReverbEffect;
+        public static PeakMeter AudioPeakMeter = null;
+        public static DmoFlangerEffect FlangerEffect;
+        public static DmoEchoEffect EchoEffect;
+        public static DmoChorusEffect ChorusEffect;
+        //public static DmoEchoEffect DCE { get { return EchoEffect; } private set { EchoEffect = value; } }
+        public static DmoGargleEffect GargleEffect;
+        public static PitchShifter PitchShiftEffect;
         public static GainSource GainSource;
-        public static SingleBlockNotificationStream singleBlockNotificationStream;
-        public static SimpleNotificationSource simpleNotificationSource;
+        public static VolumeSource VolumeSource;
+        public static AudioMeterInformation MeterInformation;
+        public static AudioSessionManager2 AudioSessionManager;
+        public static AudioSessionEnumerator SessionEnumerator;
+
+        public static SingleBlockNotificationStream SingleBlockNotificationStream;
+        public static SimpleNotificationSource SimpleNotificationSource;
         public static NotificationSource NotificationSource;
         public static string[] ValidBitrates;
-        public static AudioMeterInformation MeterInformation;
         public static CSCore.DSP.FftProvider FftProvider;
-        //internal static NotificationSource notificationSource;
     }
 }
