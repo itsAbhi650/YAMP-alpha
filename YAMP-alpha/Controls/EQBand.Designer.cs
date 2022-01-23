ff2746407bde69d39f7cbf82816581b01d077159
itsAbhi650 <abhinavp650@gmail.com>
2022-01-23 23:17:23 +0530

    Created custom control EQBand for Equalizer

---
+++ YAMP-alpha/Controls/EQBand.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace YAMP_alpha.Controls
+{
+    partial class EQBand
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EQBox = new System.Windows.Forms.GroupBox();
+            this.EQBandBar = new System.Windows.Forms.TrackBar();
+            this.EQSideMargin = new System.Windows.Forms.Panel();
+            this.EQFooter = new System.Windows.Forms.Label();
+            this.EQBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EQBandBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EQBox
+            // 
+            this.EQBox.Controls.Add(this.EQBandBar);
+            this.EQBox.Controls.Add(this.EQSideMargin);
+            this.EQBox.Controls.Add(this.EQFooter);
+            this.EQBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EQBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EQBox.Location = new System.Drawing.Point(0, 0);
+            this.EQBox.Name = "EQBox";
+            this.EQBox.Padding = new System.Windows.Forms.Padding(1);
+            this.EQBox.Size = new System.Drawing.Size(72, 182);
+            this.EQBox.TabIndex = 0;
+            this.EQBox.TabStop = false;
+            // 
+            // EQBandBar
+            // 
+            this.EQBandBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EQBandBar.Location = new System.Drawing.Point(17, 14);
+            this.EQBandBar.Name = "EQBandBar";
+            this.EQBandBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.EQBandBar.Size = new System.Drawing.Size(54, 152);
+            this.EQBandBar.TabIndex = 2;
+            this.EQBandBar.TickFrequency = 5;
+            this.EQBandBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.EQBandBar.ValueChanged += new System.EventHandler(this.EQBandBar_ValueChanged);
+            this.EQBandBar.Enter += new System.EventHandler(this.EQBandBar_Enter);
+            this.EQBandBar.ChangeUICues += new System.Windows.Forms.UICuesEventHandler(this.EQBandBar_ChangeUICues);
+            // 
+            // EQSideMargin
+            // 
+            this.EQSideMargin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.EQSideMargin.Location = new System.Drawing.Point(1, 14);
+            this.EQSideMargin.Name = "EQSideMargin";
+            this.EQSideMargin.Size = new System.Drawing.Size(16, 152);
+            this.EQSideMargin.TabIndex = 0;
+            // 
+            // EQFooter
+            // 
+            this.EQFooter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EQFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.EQFooter.Location = new System.Drawing.Point(1, 166);
+            this.EQFooter.Name = "EQFooter";
+            this.EQFooter.Size = new System.Drawing.Size(70, 15);
+            this.EQFooter.TabIndex = 1;
+            this.EQFooter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.EQFooter.Click += new System.EventHandler(this.EQFooter_Click);
+            // 
+            // EQBand
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.EQBox);
+            this.Name = "EQBand";
+            this.Size = new System.Drawing.Size(72, 182);
+            this.SizeChanged += new System.EventHandler(this.EQBand_SizeChanged);
+            this.Enter += new System.EventHandler(this.EQBand_FocusChanged);
+            this.Leave += new System.EventHandler(this.EQBand_FocusChanged);
+            this.EQBox.ResumeLayout(false);
+            this.EQBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EQBandBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox EQBox;
+        private System.Windows.Forms.TrackBar EQBandBar;
+        private System.Windows.Forms.Panel EQSideMargin;
+        private System.Windows.Forms.Label EQFooter;
+    }
+}
---
+++ YAMP-alpha/Controls/EQBand.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YAMP_alpha.Controls
+{
+    public partial class EQBand : UserControl
+    {
+        public EQBand()
+        {
+            InitializeComponent();
+        }
+
+        public EQBand(string Header, int MinVal, int MaxVal, int Value, string Footer)
+        {
+            InitializeComponent();
+            EQBox.Text = Header;
+            BandMin = MinVal;
+            BandMax = MaxVal;
+            bool flag = Value >= MinVal && Value < MaxVal;
+            if (flag)
+            {
+                BandValue = Value;
+            }
+            else
+            {
+                BandValue = MinVal;
+            }
+            FooterText = Footer;
+        }
+
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public int BandMin
+        {
+            get
+            {
+                return EQBandBar.Minimum;
+            }
+            set
+            {
+                EQBandBar.Minimum = value;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public int BandMax
+        {
+            get
+            {
+                return EQBandBar.Maximum;
+            }
+            set
+            {
+                EQBandBar.Maximum = value;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public int BandValue
+        {
+            get
+            {
+                return EQBandBar.Value;
+            }
+            set
+            {
+                EQBandBar.Value = value;
+                OnValueChanged();
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public string FooterText
+        {
+            get
+            {
+                return EQFooter.Text;
+            }
+            set
+            {
+                EQFooter.Text = value;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public TrackBar Tracker
+        {
+            get
+            {
+                return EQBandBar;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public Panel MarginPanel
+        {
+            get
+            {
+                return EQSideMargin;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public TickStyle TickStyle
+        {
+            get
+            {
+                return EQBandBar.TickStyle;
+            }
+            set
+            {
+                EQBandBar.TickStyle = value;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public int TickFrequency
+        {
+            get
+            {
+                return EQBandBar.TickFrequency;
+            }
+            set
+            {
+                EQBandBar.TickFrequency = value;
+            }
+        }
+
+        [Category("EQBand Settings")]
+        [Browsable(true)]
+        public bool FooterVisible
+        {
+            get
+            {
+                return EQFooter.Visible;
+            }
+            set
+            {
+                EQFooter.Visible = value;
+            }
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public override string Text
+        {
+            get
+            {
+                return EQBox.Text;
+            }
+            set
+            {
+                EQBox.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        public event EventHandler ValueChanged;
+        private void OnValueChanged()
+        {
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+       
+        [Browsable(true)]
+        public new event EventHandler TextChanged;
+        private void OnTextChanged()
+        {
+            TextChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void EQBandBar_ValueChanged(object sender, EventArgs e)
+        {
+            BandValue = EQBandBar.Value;
+        }
+
+        private void EQBand_SizeChanged(object sender, EventArgs e)
+        {
+            EQSideMargin.Width = base.ClientRectangle.Width / 2 - 22;
+        }
+
+        private void EQBand_FocusChanged(object sender, EventArgs e)
+        {
+            Color _color = EQFooter.BackColor;
+            EQFooter.BackColor = EQFooter.ForeColor;
+            EQFooter.ForeColor = _color;
+        }
+
+        private void EQFooter_Click(object sender, EventArgs e)
+        {
+            Focus();
+        }
+
+        private void EQBandBar_ChangeUICues(object sender, UICuesEventArgs e)
+        {
+
+        }
+
+        private void EQBandBar_Enter(object sender, EventArgs e)
+        {
+            Focus();
+        }
+    }
+}
