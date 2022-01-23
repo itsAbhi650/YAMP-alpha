df85fd3fa1d63c01b2b2994aafb6d3eeb9b3187e
itsAbhi650 <abhinavp650@gmail.com>
2022-01-23 18:59:14 +0530

    Added new class 'YAMPVars' which will now contain all the necessary object required by 'YAMP-Core'

---
+++ YAMP-alpha/Variables.cs
@@ -0,0 +1,20 @@
+﻿using CSCore.Streams;
+using CSCore;
+using CSCore.CoreAudioAPI;
+
+namespace YAMP_alpha
+{
+    public static class YAMPVars
+    {
+        public static YAMP_Core CORE;
+        public static VolumeSource VolumeSource;
+        public static GainSource GainSource;
+        public static SingleBlockNotificationStream singleBlockNotificationStream;
+        public static SimpleNotificationSource simpleNotificationSource;
+        public static NotificationSource NotificationSource;
+        public static string[] ValidBitrates;
+        public static AudioMeterInformation MeterInformation;
+        public static CSCore.DSP.FftProvider FftProvider;
+        //internal static NotificationSource notificationSource;
+    }
+}
