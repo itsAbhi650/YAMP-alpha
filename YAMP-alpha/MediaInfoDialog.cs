cec9dc05e3fa58439106e9332a0a3a0fa3868344
itsAbhi650 <abhinavp650@gmail.com>
2022-01-30 12:09:45 +0530

    Added MediaInfo Information Dialog for Tracks

---
+++ YAMP-alpha/MediaInfoDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YAMP_alpha
+{
+    public partial class MediaInfoDialog : Form
+    {
+        public MediaInfoDialog(string TrackPath)
+        {
+            InitializeComponent();
+            
+            MediaInfo.MediaInfoWrapper minfo = new MediaInfo.MediaInfoWrapper(TrackPath);
+            textBox1.Text = minfo.Text.TrimEnd('\n', ' ');
+        }
+    }
+}
