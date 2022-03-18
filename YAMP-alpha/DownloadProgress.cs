fe7669df802e24d43575475585fb0ce185032b47
itsAbhi650 <abhinavp650@gmail.com>
2022-03-18 12:50:47 +0530

    DownloadProgress shows current progress of a stream being downloaded offline.

---
+++ YAMP-alpha/DownloadProgress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace YAMP_alpha
+{
+    public partial class DownloadProgress : Form
+    {
+        public int Percent { get { return progressBar1.Value; } set { progressBar1.Value = value; OnPercentChanged(); } }
+
+        public DownloadProgress()
+        {
+            InitializeComponent();
+            Percent = 0;
+            PercentChanged += DownloadProgress_PercentChanged;
+        }
+
+        private void DownloadProgress_PercentChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public event EventHandler PercentChanged;
+        private void OnPercentChanged()
+        {
+            PercentChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
