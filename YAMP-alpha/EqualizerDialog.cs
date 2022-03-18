ab3c81e416e0edbd91edf11f39437b0004236fc2
itsAbhi650 <abhinavp650@gmail.com>
2022-03-18 13:14:06 +0530

    Removed comment

--- YAMP-alpha/EqualizerDialog.cs
+++ YAMP-alpha/EqualizerDialog.cs
@@ -98,7 +98,6 @@ namespace YAMP_alpha
                 {
                     item.BringToFront();
                 }
-
             }
         }
 
@@ -135,7 +134,6 @@ namespace YAMP_alpha
         {
             using (Graphics g = Graphics.FromImage(SpectroBitmap))
             {
-                //CachedSoundSource
                 pictureBox1.Image = null;
                 bool flag = Spectrum.CreateVoicePrint3D(g, new RectangleF(0f, 0f, SpectroBitmap.Width, SpectroBitmap.Height), _xpos, Color.Black, 3f);
                 if (flag)
