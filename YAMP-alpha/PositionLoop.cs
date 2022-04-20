9a62d2476642dd95724433df29349929e28c1bfc
itsAbhi650 <abhinavp650@gmail.com>
2022-04-20 22:17:35 +0530

    Add: class PositionLoop to support looping between two positions

---
+++ YAMP-alpha/PositionLoop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAMP_alpha
+{
+    public class PositionLoop
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public PositionLoop()
+        {
+            A = 0;
+            B = 0;
+        }
+    }
+}
